1d23e9bbe7685f0cf72115e1544be4964b5308c2
Shubhan46 <77258027+Shubhan46@users.noreply.github.com>
2021-02-24 20:19:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 // This is the code for canvas variables
 canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
 //this code is for drawing a rectangle
 ctx.beginPath();
-ctx.strokeStyle="red";
+ctx.strokeStyle="grey";
 ctx.lineWidth=2;
 ctx.rect(150, 143, 430, 200);
 ctx.stroke();
@@ -11,29 +12,29 @@ ctx.stroke();
 ctx.beginPath();
 ctx.strokeStyle="yellow";
 ctx.lineWidth=2;
-ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.arc(250, 210, 40, 0, 2*Math.PI);
 ctx.stroke();
 //this code is for drawing another circle(for the logo of the olympic!)
 ctx.beginPath();
 ctx.strokeStyle="green";
 ctx.lineWidth=2;
-ctx.arc(300, 300, 40, 0, 2*Math.PI);
+ctx.arc(350, 210, 40, 0, 2*Math.PI);
 ctx.stroke();
 //this code is for drawing another circle(for the logo of the olympic!)
 ctx.beginPath();
 ctx.strokeStyle="red";
 ctx.lineWidth=2;
-ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.arc(450, 210, 40, 0, 2*Math.PI);
 ctx.stroke();
 //this code is for drawing another circle(for the logo of the olympic!)
 ctx.beginPath();
 ctx.strokeStyle="blue";
 ctx.lineWidth=2;
-ctx.arc(300, 300, 40, 0, 2*Math.PI);
+ctx.arc(300, 250, 40, 0, 2*Math.PI);
 ctx.stroke();
 //this code is for drawing another circle(for the logo of the olympic!)
 ctx.beginPath();
 ctx.strokeStyle="orange";
 ctx.lineWidth=2;
-ctx.arc(200, 200, 40, 0, 2*Math.PI);
+ctx.arc(400, 250, 40, 0, 2*Math.PI);
 ctx.stroke();
